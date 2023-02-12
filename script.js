21ff81d02a6acdb3fc5cb124fafa721d459baf65
Наталья <0207natalya@gmail.com>
2023-02-12 21:16:03 +0600

    submit

--- script.js
+++ script.js
@@ -42,3 +42,15 @@ function calculate() {
     res3 * parseInt(costFuel.value) * parseInt(consumption.value)
   );
 }
+
+const form = document.querySelector("form");
+const formSubmit = document.querySelector("button");
+
+form.addEventListener("change", changeFormHandler);
+
+function changeFormHandler() {
+  console.log(form.checkValidity());
+  if (form.checkValidity()) {
+    formSubmit.removeAttribute("disabled");
+  }
+}
